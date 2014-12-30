8eac2005f735af2ce0e4dd6255c148416728fc07
War777 <oscarResendizUaq@gmail.com>
2014-12-29 18:36:35 -0600

    En Construccion

--- index.php
+++ index.php
@@ -1,3 +1,84 @@
-<h1>
-	Das Right My Nigga
-</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Ceramica Servin</title>
+
+	<link rel="stylesheet" href="c/bootstrap.css">
+</head>
+<body>
+	
+	<p></p><p></p>
+
+	<div class="row">
+		<div class="col-lg-12 text-center">
+			<img src="i/brand.jpg" class="text-center">
+		</div>
+	</div>
+	
+	<div class="footer">
+
+		<div class="row" style="padding:15px;">
+			<div class="col-md-4">
+				<h4>
+					<b>
+						THOMAS CASTRO SERVIN SOTO
+						<br>
+						EVA SERVIN MORALES
+					</b>
+				</h4>
+				<p><b>Artesanos</b></p>
+
+				<h4>
+					<b>
+						CALLE LA CRUZ 26 BO. STA. MAR&Iacute;A
+						<br>VILLA PROGRESO C.P. 76670
+						<br>EZEQUIEL MONTES, QUER&Eacute;TARO
+						<br>CEL: 427 427 1344
+					</b>
+				</h4>
+
+				<p><b>servinceramicaqro@hotmail.com</b></p>
+
+			</div>
+
+			<div class="col-md-4 col-md-offset-4 text-center">
+				<img src="i/drop.jpg" alt="">
+				<p>
+					<b>
+						Imagenes <br> Productos
+					</b>
+				</p>
+			</div>
+		</div>
+
+	</div>
+	
+</body>
+</html>
+
+<script src="j/jquery-2.1.1.js"></script>
+<script src="j/bootstrap.js"></script>
+
+<style>
+	html, body{
+		background-color: #00235D;
+		color: #ffffff;
+	}
+
+	.footer{
+		z-index:100;
+		position: fixed;
+		bottom: 0px;
+		width: 100%;
+	}
+
+	p{
+		color: #ffffff;
+	}
+
+	.withBg{
+		background: rgba(0, 0, 0, 0.2);
+	}
+
+</style>
